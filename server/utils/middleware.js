1e791f86d2d0757d0b545cdbc2c3c1b562dd9786
Amir thapa magar <amirthapa2019@gmail.com>
2022-12-19 16:37:32 +0545

    feat:console cleared middleware

--- server/utils/middleware.js
+++ server/utils/middleware.js
@@ -2,8 +2,6 @@ const jwt = require("jsonwebtoken");
 const config = require("../utils/config");
 
 const tokenExtractor = (req, res, next) => {
-  // console.log("the tokenExtractor rneterd");
-
   const authorization = req.get("authorization");
   if (authorization && authorization.toLowerCase().startsWith("bearer ")) {
     req.token = authorization.substring(7);
@@ -12,11 +10,9 @@ const tokenExtractor = (req, res, next) => {
 };
 
 const userExtractor = (req, res, next) => {
-  // console.log("the userextrracrorte rneterd");
   const authorization = req.get("authorization");
   if (authorization && authorization.toLowerCase().startsWith("bearer ")) {
     const decodedToken = jwt.verify(req.token, config.SECRET);
-    // console.log("the tokenExtractor in", decodedToken);
     if (!decodedToken.id) {
       res.status(401).json({ error: "token missing or invalid" });
     }
